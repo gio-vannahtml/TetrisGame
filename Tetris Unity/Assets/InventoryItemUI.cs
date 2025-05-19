6467ae0d344db7df7be8993028c93419284966fd
AniBustos <anakbustos@gmail.com>
2025-05-19 18:11:53 +0200

    items working
    We have to check them up based on the level

---
+++ Tetris Unity/Assets/InventoryItemUI.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class InventoryItemUI : MonoBehaviour
+{
+    public enum ItemType { Bombastic, Crusher, Tractor, ColorPopper }
+    public ItemType itemType;
+
+    public GridScript gridScript;
+
+    public void UseItem()
+    {
+        switch (itemType)
+        {
+            case ItemType.Bombastic:
+                gridScript.UseBombastic(new Vector2(gridScript.width / 2, gridScript.height / 2));
+                break;
+            case ItemType.Crusher:
+                gridScript.UseCrusher();
+                break;
+            case ItemType.Tractor:
+                gridScript.UseTractor();
+                break;
+            case ItemType.ColorPopper:
+                gridScript.UseColorPopper();
+                break;
+        }
+
+        Debug.Log($"{itemType} used!");
+        Destroy(gameObject); // Optional: remove item after use
+    }
+}
