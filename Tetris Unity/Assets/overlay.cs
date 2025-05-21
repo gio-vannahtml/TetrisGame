8e6a703430ef50691e6b6f09151d7cfbc5e5c34c
AniBustos <anakbustos@gmail.com>
2025-05-21 20:42:56 +0200

    overlays boss

--- Tetris Unity/Assets/overlay.cs
+++ Tetris Unity/Assets/overlay.cs
@@ -9,6 +9,7 @@ public class Overlay : MonoBehaviour
 
 void Start()
     {
+        Time.timeScale = 0f;
         StartCoroutine(ShowOverlays());
     }
 
@@ -16,20 +17,21 @@ void Start()
     {
         // Show overlay 1
         overlay1.SetActive(true);
-        yield return new WaitForSeconds(3f);
+        yield return new WaitForSecondsRealtime(2f);
         overlay1.SetActive(false);
 
         // Show overlay 2
         overlay2.SetActive(true);
-        yield return new WaitForSeconds(3f);
+        yield return new WaitForSecondsRealtime(2f);
         overlay2.SetActive(false);
 
         // Show overlay 3
         overlay3.SetActive(true);
-        yield return new WaitForSeconds(3f);
+        yield return new WaitForSecondsRealtime(2f);
         overlay3.SetActive(false);
 
         // Now game starts (you can call any game start logic here)
+        Time.timeScale = 1f;
         Debug.Log("Game starts!");
     }
 }
