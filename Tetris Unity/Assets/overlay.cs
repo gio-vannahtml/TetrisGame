16317defdefa5f6059c1496ff299812b52dfbbda
AniBustos <anakbustos@gmail.com>
2025-05-23 09:33:52 +0200

    overlay change script

--- Tetris Unity/Assets/overlay.cs
+++ Tetris Unity/Assets/overlay.cs
@@ -3,8 +3,6 @@ using UnityEngine;
 
 public class Overlay : MonoBehaviour
 {
-    public GameObject tutorialOverlay1;
-    public GameObject tutorialOverlay2;
     public GameObject overlay1;
     public GameObject overlay2;
     public GameObject overlay3;
@@ -19,17 +17,6 @@ public class Overlay : MonoBehaviour
     {
         // Pause the game globally
         Time.timeScale = 0f;
-
-        // Show tutorial overlay 1
-        tutorialOverlay1.SetActive(true);
-        yield return new WaitForSecondsRealtime(2f);
-        tutorialOverlay1.SetActive(false);
-
-        // Show tutorial overlay 2
-        tutorialOverlay2.SetActive(true);
-        yield return new WaitForSecondsRealtime(2f);
-        tutorialOverlay2.SetActive(false);
-
         // Show overlay 1
         overlay1.SetActive(true);
         yield return new WaitForSecondsRealtime(2f);
